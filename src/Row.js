bccabf8d162c831f29bbc1a756c589e1534bf78f
tulsi <kumari20tulsi@gmail.com>
2024-06-20 00:26:47 +0530

    removing trailers

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,77 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from './axios';
+// import './Row.css';
+// import YouTube from 'react-youtube';
+// import movieTrailer from 'movie-trailer';
+
+// function Row({ title, fetchUrl, isLargeRow }) {
+//     const [movies, setMovies] = useState([]);
+//     const [trailerUrl, setTrailerUrl] = useState("");
+
+//     useEffect(() => {
+//         async function fetchData() {
+//             const request = await axios.get(fetchUrl);
+//             setMovies(request.data.Search); // OMDB uses 'Search' to store the results
+//             return request;
+//         }
+//         fetchData();
+//     }, [fetchUrl]);
+
+//     const opts = {
+//         height: "390",
+//         width: "100%",
+//         playerVars: {
+//             autoplay: 1,
+//         },
+//     };
+
+//     const handleClick = (movie) => {
+//         if (trailerUrl) {
+//             setTrailerUrl("");
+//         } else {
+//             movieTrailer(movie?.Title || "")
+//                 .then((url) => {
+//                     if (url) {
+//                         const urlParams = new URLSearchParams(new URL(url).search);
+//                         setTrailerUrl(urlParams.get('v'));
+//                     } else {
+//                         console.error('No trailer URL found');
+//                     }
+//                 })
+//                 .catch((error) => {
+//                     console.error('Error fetching trailer:', error);
+//                 });
+//         }
+//     };
+
+//     return (
+//         <div className='row'>
+//             <h2>{title}</h2>
+//             <div className='row_posters'>
+//                 {movies.map(movie => (
+//                     <img
+//                         key={movie.imdbID}
+//                         onClick={() => handleClick(movie)}
+//                         className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+//                         src={movie.Poster !== "N/A" ? movie.Poster : ""}
+//                         alt={movie.Title}
+//                     />
+//                 ))}
+//             </div>
+//             {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+//         </div>
+//     );
+// }
+
+// export default Row;
+
+// Row.js
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
-import YouTube from 'react-youtube';
-import movieTrailer from 'movie-trailer';
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
-    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
         async function fetchData() {
@@ -17,33 +82,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData();
     }, [fetchUrl]);
 
-    const opts = {
-        height: "390",
-        width: "100%",
-        playerVars: {
-            autoplay: 1,
-        },
-    };
-
-    const handleClick = (movie) => {
-        if (trailerUrl) {
-            setTrailerUrl("");
-        } else {
-            movieTrailer(movie?.Title || "")
-                .then((url) => {
-                    if (url) {
-                        const urlParams = new URLSearchParams(new URL(url).search);
-                        setTrailerUrl(urlParams.get('v'));
-                    } else {
-                        console.error('No trailer URL found');
-                    }
-                })
-                .catch((error) => {
-                    console.error('Error fetching trailer:', error);
-                });
-        }
-    };
-
     return (
         <div className='row'>
             <h2>{title}</h2>
@@ -51,16 +89,15 @@ function Row({ title, fetchUrl, isLargeRow }) {
                 {movies.map(movie => (
                     <img
                         key={movie.imdbID}
-                        onClick={() => handleClick(movie)}
                         className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                         src={movie.Poster !== "N/A" ? movie.Poster : ""}
                         alt={movie.Title}
                     />
                 ))}
             </div>
-            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 }
 
 export default Row;
+
